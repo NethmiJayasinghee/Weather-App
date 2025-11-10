d94a1e493b99dd8ec95c52503bf77e60b2dab8b6
NethmiJayasinghee <ndhananjana98@gmail.com>
2025-11-09 22:40:52 -0800

    Code App.jsx and App.css

--- Weather-app/src/App.jsx
+++ Weather-app/src/App.jsx
@@ -2,35 +2,17 @@ import React from "react";
 import Header from './Header';
 import Footer from './Footer';
 
+
 function App() {
   return (
     <div>
       <Header />
       <main style={{ padding: "40px", textAlign: "center" }}>
+        <h2 className="topic">Check the weather in your city</h2>
+        <input type="text"placeholder="Enter city name" />
         <h2>Check the weather in your city</h2>
-        <input
-          type="text"
-          placeholder="Enter city name"
-          style={{
-            padding: "10px",
-            width: "250px",
-            border: "1px solid #ccc",
-            borderRadius: "5px",
-            marginRight: "10px",
-          }}
-        />
-        <button
-          style={{
-            padding: "10px 20px",
-            backgroundColor: "#007BFF",
-            color: "white",
-            border: "none",
-            borderRadius: "5px",
-            cursor: "pointer",
-          }}
-        >
-          Search
-        </button>
+        <input type="text" placeholder="Enter city name" />
+        <button>Search</button>
       </main>
       <Footer />
     </div>
